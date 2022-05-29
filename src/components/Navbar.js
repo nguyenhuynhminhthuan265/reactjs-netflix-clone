9b6694940490d939d004077dccb2fc156ae7a09b
minhthuanit <nguyenthuanit265@gmail.com>
2022-05-29 15:53:11 +0700

    commit 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
 function Navbar() {
+  console.log(process.env.PUBLIC_UR);
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+  });
   return (
-    <div>Navbar</div>
-  )
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src={process.env.PUBLIC_URL + "/images/netflix.png"}
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src={process.env.PUBLIC_URL + "/images/logo192.png"}
+        alt="Netflix Logo"
+      />
+    </div>
+  );
 }
-
-export default Navbar
+export default Navbar;
